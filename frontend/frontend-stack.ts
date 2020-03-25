ff9bed1fbf306d46344e42e0d10bba80a0bfa966
Vo, Hieu <Hieu.Vo@coxautoinc.com>
2020-03-25 15:30:04 -0700

    Add frontend-stack resources

--- frontend/frontend-stack.ts
+++ frontend/frontend-stack.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env ts-node
 import { Stack, App, StackProps } from '@aws-cdk/core';
 import { StringParameter } from '@aws-cdk/aws-ssm';
 import { CloudFrontS3StackBuilder } from 'aws-cdk-stack-builders/stack-builders/frontend/CloudFrontS3StackBuilder';
@@ -10,7 +9,7 @@ export class FrontEndStack extends Stack {
     const accountId = Stack.of(this).account;
     const region = Stack.of(this).region;
 
-    const recordName = 'gatsby-stack';
+    const recordName = 'frontend-template';
     const domainName = 'labtest44.com';
     const s3OriginPath = '/green'
     const acmCertRef = StringParameter.valueForStringParameter(this, '/cert/com-labtest44');
@@ -19,7 +18,6 @@ export class FrontEndStack extends Stack {
     frontendStackBuilder
       .createOriginAccessIdentity()
       .createS3Bucket(`${region}-${recordName}-${accountId}`)
-      // .createDistributionForS3({ s3OriginPath: "/blue" });
       .createDistributionForS3WithAlias({
         acmCertRef,
         s3OriginPath,
@@ -31,13 +29,8 @@ export class FrontEndStack extends Stack {
 
 const app = new App();
 
-// const domain = app.node.tryGetContext('domain');
-// const subdomain = app.node.tryGetContext('subdomain');
-
-new FrontEndStack(app, 'FrontEndStack', {
+new FrontEndStack(app, 'TemplateFrontEndStack', {
   env: {
-    // Stack must be in us-east-1, because the ACM certificate for a
-    // global CloudFront distribution must be requested in us-east-1.
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: 'us-east-1'
   }
